d3d0168b2bb6fe3496532e3dbc6acb62e0d71c02
Blazeleo <satviksams@gmail.com>
2021-06-25 15:22:34 +0530

    Pushing bot files

---
+++ nadekobot-1.9/NadekoBot.Core/Services/Database/Models/CustomReaction.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace NadekoBot.Core.Services.Database.Models
+{
+    public class CustomReaction : DbEntity
+    {
+
+        [NotMapped]
+        public Regex Regex { get; set; }
+        public ulong UseCount { get; set; }
+        public bool IsRegex { get; set; }
+        public bool OwnerOnly { get; set; }
+        
+        public ulong? GuildId { get; set; }
+        public string Response { get; set; }
+        public string Trigger { get; set; }
+
+        public bool AutoDeleteTrigger { get; set; }
+        public bool DmResponse { get; set; }
+        public bool ContainsAnywhere { get; set; }
+        public bool AllowTarget { get; set; }
+        public string Reactions { get; set; }
+
+        public string[] GetReactions() =>
+            Array.Empty<string>();
+        
+        public bool IsGlobal() => GuildId is null || GuildId == 0;
+    }
+
+    public class ReactionResponse : DbEntity
+    {
+        public bool OwnerOnly { get; set; }
+        public string Text { get; set; }
+    }
+}
